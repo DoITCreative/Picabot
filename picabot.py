eb75ecf1f77567d084fd6a636578b820d7b44076
Ivan Raiskiy <ivanrisky@mail.ru>
2016-10-06 00:27:36 +0200

    Adds engine

--- picabot.py
+++ picabot.py
@@ -9,8 +9,18 @@ from kivy.uix.image import Image
 from kivy.uix.progressbar import ProgressBar
 from kivy.lang import Builder
 
+import requests
+import sys
+import time
+import random
+
+postid="4526079"
+likeornot="1"
+loginfilepath="logins.txt"
+proxyfilepath="proxies.txt"
+
 kv="""
-<ContainerBox>
+<MainMenu>
     BoxLayout:
         orientation:'vertical'
         padding:0,0,0,10
@@ -110,51 +120,127 @@ kv="""
             pos_hint:{'center_x':.5}
             size_hint:.1,.1
         ProgressBar:
+            id:pBar
             max:100
             pos_hint:{'center_x':.5}
             size_hint:.7,.3
 """
 Builder.load_string(kv)
 
-class ContainerBox(BoxLayout):
+class MainMenu(BoxLayout):
     def __init__(self, **kwargs):
         Config.set('input','mouse','mouse,multitouch_on_demand')
         Config.set('graphics','resizable',0)
         Config.set('graphics','width',800)
         Config.set('graphics','height',600)
-        super(ContainerBox, self).__init__(**kwargs)
+        super(MainMenu, self).__init__(**kwargs)
 
     def bProxieOnClick(self):
-        print("Proxie")
         self.ids.lDone1.opacity=1
     def bAccontsOnClick(self):
-        print("Accounts")
         self.ids.lDone2.opacity=1
     def bIdOnClick(self):
-        print("ID")
         self.ids.lDone3.opacity=1
     def bHelpOnClick(self):
-        print("help")
         if (self.ids.iHelp.opacity==1):
             self.ids.iHelp.opacity=0
         else:
             self.ids.iHelp.opacity=1
 
     def bLoveOnClick(self):
-        print("love")
+        likeornot="1"
+        self.ids.lOut.text="[color=69d369]Selected option: Love[/color]"
     def bHateOnClick(self):
-        print("hate")
+        likeornot="-1"
+        self.ids.lOut.text="[color=69d369]Selected option: Hate[/color]"
     def bCookieOnClick(self):
-        print("cookie")
+        loginfile = open(loginfilepath,'r')
+        proxyfile = open(proxyfilepath,'r')
+        logins=[]
+        passwords=[]
+        proxys=[]
+        proxystype=[]
+        count=0
+        loginsnum=0
+        proxysnum=0
 
-class Engine:
-    def __init__(self, **kwargs):
-        print ("Start of the program")
+        for line in loginfile:
+            s = line
+            s1,s2 = s.split(':')
+            logins.append(s1)
+            passwords.append(s2[:-1])
+        for line in proxyfile:
+            s = line
+            s1,s2 = s.split()
+            proxystype.append(s1)
+            proxys.append(s2[:-1])
+        for line in logins:
+            loginsnum=loginsnum+1
+        for line in proxys:
+            proxysnum=proxysnum+1
+        if proxysnum>=loginsnum:
+            count=loginsnum
+        else:
+            count=proxysnum
+        self.ids.pBar.max=count
+        self.ids.pBar.value=0
+        for k in range(0,count):
+#            sendreq(logins[k],passwords[k],str(postid),proxystype[k],proxys[k],str(likeornot))
+            self.ids.pBar.value=self.ids.pBar.value+1
+            time.sleep(random.random()+int(random.random()*2)+1)
+        self.ids.lOut.text="[color=69d369]Done![/color]"
+
+    def sendreq(login, password, storyid, proxytype, proxy, vote):
+        mproxies = {
+                proxytype : proxytype+"://"+proxy
+                }
+        mheaders = {
+                'Host':'pikabu.ru',
+                'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0',
+                'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+                'Accept':'application/json, text/javascript, */*; q=0.01',
+                'Accept-Language':'en-US,en;q=0.5',
+                'Accept-Encoding':'gzip, deflate',
+                'X-CSRF-Token':'t5fhrka497b005npsh387ffaj0rk5f7i',
+                'x-requested-with':'x-requested-with: XMLHttpRequest',
+                'Content-Length':str(69+len(login))
+                }
+        mdata = {
+                "username":login,
+                "password":password,
+                "g-recaptcha-response":"",
+                "mode":"login"
+                }
+        m2data = {
+                "story_id":storyid,
+                "vote":vote
+                }
+        m1cookies = {
+                'PHPSESS':'t5fhrka497b005npsh387ffaj0rk5f7i',
+                'is_scrollmode':'1',
+                'autohide_news':'0',
+                '_nuser':'1',
+                'nps7s':'2148041329',
+                'vn_buff':'[%22190f158bef%22%2C[]]',
+                'la':'1',
+                'set_autohide_news':'-1'
+                }
+        if proxy!="none":
+            r = requests.post("http://pikabu.ru/ajax/auth.php", proxies=mproxies, data=mdata,headers=mheaders,cookies=m1cookies)
+        else:
+            r = requests.post("http://pikabu.ru/ajax/auth.php", data=mdata,headers=mheaders,cookies=m1cookies)
+        m2cookies = m1cookies
+        m2cookies['phpDug2']=r.cookies.get_dict()['phpDug2']
+        if proxy!="none":
+            r1 = requests.post("http://pikabu.ru/ajax/vote_story.php", data=m2data, proxies=mproxies, headers=mheaders, cookies=m2cookies) 
+        else:
+            r1 = requests.post("http://pikabu.ru/ajax/vote_story.php", data=m2data, headers=mheaders, cookies=m2cookies) 
+        return 
 
 class PicabotApp(App):
     
     def build(self):
-        return ContainerBox()
+        return MainMenu()
 
 if __name__ == "__main__":
     PicabotApp().run()
